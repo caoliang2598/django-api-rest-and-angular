dba6495408fa732e367c4e81ce6656c31498b062
caoliang2598 <caoliang2598@hotmail.com>
2014-04-27 00:43:20 +0200

    Made for the for TDDD07

---
+++ example/api/migrations/0002_auto__add_field_user_age__add_field_user_sex__add_field_user_proPic.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'User.age'
+        db.add_column(u'api_user', 'age',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=0),
+                      keep_default=False)
+
+        # Adding field 'User.sex'
+        db.add_column(u'api_user', 'sex',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=20),
+                      keep_default=False)
+
+        # Adding field 'User.proPic'
+        db.add_column(u'api_user', 'proPic',
+                      self.gf('django.db.models.fields.files.ImageField')(default='', max_length=100),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'User.age'
+        db.delete_column(u'api_user', 'age')
+
+        # Deleting field 'User.sex'
+        db.delete_column(u'api_user', 'sex')
+
+        # Deleting field 'User.proPic'
+        db.delete_column(u'api_user', 'proPic')
+
+
+    models = {
+        u'api.photo': {
+            'Meta': {'object_name': 'Photo'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'photos'", 'to': u"orm['api.Post']"})
+        },
+        u'api.post': {
+            'Meta': {'object_name': 'Post'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'posts'", 'to': u"orm['api.User']"}),
+            'body': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'api.user': {
+            'Meta': {'object_name': 'User'},
+            'age': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'followers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'followees'", 'symmetrical': 'False', 'to': u"orm['api.User']"}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'proPic': ('django.db.models.fields.files.ImageField', [], {'default': "''", 'max_length': '100'}),
+            'sex': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '20'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['api']
--- example/api/models.py
+++ example/api/models.py
@@ -4,15 +4,28 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-	followers = models.ManyToManyField('self', related_name='followees', symmetrical=False)
+    age = models.PositiveIntegerField(default=0)
+    sex = models.CharField(max_length=20, default="")
+    desc = models.CharField(max_length=100,default="")
+    followers = models.ManyToManyField('self', related_name='followees', symmetrical=False)
+    proPic = models.ImageField(upload_to="%Y/%m/%d", default="")
+
+
+class Plan(models.Model):
+    time = models.DateTimeField()
+    usr = models.ForeignKey(User,related_name='plan')
+    des = models.CharField(max_length=50)
+    dur = models.PositiveIntegerField(default=0)
+    flightNr = models.CharField(max_length=20)
+
 
 
 class Post(models.Model):
-	author = models.ForeignKey(User, related_name='posts')
-	title = models.CharField(max_length=255)
-	body = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(User, related_name='posts')
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True, null=True)
 
 
 class Photo(models.Model):
-	post = models.ForeignKey(Post, related_name='photos')
-	image = models.ImageField(upload_to="%Y/%m/%d")
+    post = models.ForeignKey(Post, related_name='photos')
+    image = models.ImageField(upload_to="%Y/%m/%d")
